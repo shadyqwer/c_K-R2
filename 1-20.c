e0becbc67d876e40e849030fb48f5fe675dc2794
Milos Stamenkovic <milos_stamenkovic@outlook.com>
2023-01-05 15:52:02 +0100

    Create 1-20.c

---
+++ 1-20.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define TAB_DIST 8
+
+int main(void)
+{
+    int c, pos = 0;
+    while ((c = getchar())  != '~')
+    {
+        if (c == '\t')
+        {
+            int spaces = TAB_DIST - (pos % TAB_DIST);
+            for (int i = 0; i < spaces; ++i)
+            {
+                putchar(' ');
+            }
+            pos += spaces;
+        }
+        else
+        {
+            putchar(c);
+            ++pos;
+            if (c == '\n')
+            {
+                pos = 0;
+            }
+        }
+    }
+    return 0;
+}
